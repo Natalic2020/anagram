e31b59a891c104bef3134ae416c092e546a416a0
Name <Name@cbs.com>
2020-05-08 15:32:49 +0200

    added new class Anagrams

---
+++ src/de/com/nata/mentoring1/Anagrams.java
@@ -0,0 +1,51 @@
+package de.com.nata.mentoring1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Anagrams {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.print("Input text for reverse: ");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		String inputText = reader.readLine();
+
+		String[] words = inputText.split(" ");
+		
+		StringBuffer textReverse = new StringBuffer();
+		
+		for (String word : words) {
+			
+			textReverse.append(wordReverse(word)).append(" ");
+		}
+		
+		System.out.print("Output reversed text:   ");
+		System.out.println(textReverse);
+
+	}
+
+	private static StringBuffer wordReverse(String word) {
+		StringBuffer wordReverse = new StringBuffer(word).reverse();
+
+		for (int i = word.length() - 1; i >= 0; i--) {
+
+			if (!Character.isLetter(wordReverse.charAt(i))) {
+
+				wordReverse.deleteCharAt(i);
+			}
+		}
+
+		for (int i = 0; i < word.length(); i++) {
+
+			if (!Character.isLetter(word.charAt(i))) {
+				wordReverse.insert(i, word.charAt(i));
+			}
+		}
+		
+		return wordReverse;
+	}
+	
+}
