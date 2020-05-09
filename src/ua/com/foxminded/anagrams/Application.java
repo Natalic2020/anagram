d5091dab10fcb98f164d4a1964fec60fdf6ddad9
Name <Name@cbs.com>
2020-05-09 08:06:26 +0200

    extract new class Anagram

--- src/ua/com/foxminded/anagrams/Application.java
+++ src/ua/com/foxminded/anagrams/Application.java
@@ -9,32 +9,27 @@ public class Application {
 
 	public static void main(String[] args) throws IOException {
 
-	    System.out.print("Input text for reverse: ");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	    System.out.println(Anagram("Go45od mor4ning!"));
+	    
+	}
 
-		final String inputText = reader.readLine();
+    private static String Anagram(final String text) throws IOException {
 
-		String[] words = inputText.split(" ");
-		
-		StringBuffer textReverse = new StringBuffer();
+		String[] words = text.split(" ");
 		
 		StringJoiner sj = new StringJoiner(" ");
 		
 		for (String word : words) {
 			
-			textReverse.append(wordReverse(word)).append(" ");
 			sj.add(wordReverse(word));
-			
 		}
+		return sj.toString();
+    }
+
+	private static StringBuilder wordReverse(final String word) {
+	    
+		StringBuilder wordReverse = new StringBuilder(word).reverse();
 		
-		System.out.print("Output reversed text:   ");
-		System.out.println(textReverse);
-		System.out.println(sj.toString());
-	}
-
-	private static StringBuffer wordReverse(String word) {
-		StringBuffer wordReverse = new StringBuffer(word).reverse();
-
 		for (int i = word.length() - 1; i >= 0; i--) {
 
 			if (!Character.isLetter(wordReverse.charAt(i))) {
