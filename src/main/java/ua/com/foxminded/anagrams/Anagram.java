55d7427b4c0567cd40a1e06c01615ae1d98d3b7e
Name <Name@cbs.com>
2020-05-18 14:17:41 +0200

    move to other folder

---
+++ src/main/java/ua/com/foxminded/anagrams/Anagram.java
@@ -0,0 +1,54 @@
+package main.java.ua.com.foxminded.anagrams;
+
+import java.util.StringJoiner;
+
+public class Anagram {
+
+    private static final String SPACE_SIGN = " ";
+    private static final String BLANK = "";
+
+    public String textReverseOnlyLetters(final String text) {
+
+        if (text == null) {
+            return text;
+        }
+
+        String[] words = text.split(SPACE_SIGN);
+
+        StringJoiner textReverse = new StringJoiner(SPACE_SIGN);
+
+        for (String word : words) {
+
+            textReverse.add(reverseOnlyLetters(word));
+        }
+
+        while (textReverse.length() < text.length()) {
+            textReverse.add(BLANK);
+        }
+
+        return textReverse.toString();
+    }
+
+    private StringBuilder reverseOnlyLetters(final String word) {
+
+        StringBuilder wordReverse = new StringBuilder(word).reverse();
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+
+            if (!Character.isLetter(wordReverse.charAt(i))) {
+
+                wordReverse.deleteCharAt(i);
+            }
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+
+            if (!Character.isLetter(word.charAt(i))) {
+                wordReverse.insert(i, word.charAt(i));
+            }
+        }
+
+        return wordReverse;
+    }
+}
+
---
+++ src/main/java/ua/com/foxminded/anagrams/Application.java
@@ -0,0 +1,17 @@
+package main.java.ua.com.foxminded.anagrams;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Anagram anagram = new Anagram();
+
+        String textRevers1 = anagram.textReverseOnlyLetters("Go45od mor4ning!");
+        String textRevers2 = anagram.textReverseOnlyLetters("Hal67lo 22world");
+        String textRevers3 = anagram.textReverseOnlyLetters("Goo01d eve//ning");
+
+        System.out.println(textRevers1);
+        System.out.println(textRevers2);
+        System.out.println(textRevers3);
+    }
+}
