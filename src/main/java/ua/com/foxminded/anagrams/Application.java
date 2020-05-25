87b9d2041ee8d5985c44761a8a790277cf94aa62
Name <Name@cbs.com>
2020-05-25 12:22:40 +0200

    add illegalArgumentException to application

--- src/main/java/ua/com/foxminded/anagrams/Application.java
+++ src/main/java/ua/com/foxminded/anagrams/Application.java
@@ -6,12 +6,16 @@ public class Application {
 
         Anagram anagram = new Anagram();
 
-        String textRevers1 = anagram.textReverseOnlyLetters("Go45od mor4ning!");
-        String textRevers2 = anagram.textReverseOnlyLetters("Hal67lo 22world");
-        String textRevers3 = anagram.textReverseOnlyLetters("Goo01d eve//ning");
+        String[] textForRevers = new String[] { "Go45od mor4ning!", "Hal67lo 22world", "Goo01d eve//ning" };
 
-        System.out.println(textRevers1);
-        System.out.println(textRevers2);
-        System.out.println(textRevers3);
+        for (String input : textForRevers) {
+
+            try {
+                System.out.println("Your result is: " + anagram.textReverseOnlyLetters(input));
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid  input:\"" + e.getMessage() + "\"");
+            }
+        }
     }
 }
