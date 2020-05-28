10c347b663ba6de3c1ef080c12e8d2a80fd56839
Tetiana Iefimenko (mentor) <efimenkot@gmail.com>
2020-05-28 15:04:23 +0300

    Revert "Merge branch 'develop' into 'master'"
    This reverts merge request !4

--- src/ua/com/foxminded/anagrams/Anagram.java
+++ src/ua/com/foxminded/anagrams/Anagram.java
@@ -3,40 +3,47 @@ package ua.com.foxminded.anagrams;
 import java.util.StringJoiner;
 
 public class Anagram {
+    
+    private static final String SPACE_SIGN = " ";
 
-    private final static String WORD_SEPARATOR = " ";
-
-    public String reverseOnlyLetters(final String text) {
+    public String textReverse(final String text) {
 
         if (text == null) {
-            throw new IllegalArgumentException("Null parameters are not allowed");
+            return text;
         }
 
-        final String textPlusSignAtTheEnd = text + "$";
-        String[] words = textPlusSignAtTheEnd.split(WORD_SEPARATOR);
-        StringJoiner textReverse = new StringJoiner(WORD_SEPARATOR);
-
+        String[] words = text.split(SPACE_SIGN);
+        
+        StringJoiner textReverse = new StringJoiner(SPACE_SIGN);
+        
         for (String word : words) {
-            textReverse.add(reverseOnlyLettersInWord(word));
+            
+            textReverse.add(reverseOnlyLetters(word));
         }
-        return textReverse.toString().substring(0, textReverse.length() - 1);
+
+        return textReverse.toString();
     }
-
-    private StringBuilder reverseOnlyLettersInWord(final String word) {
-
+       
+    
+    private StringBuilder reverseOnlyLetters(final String word) {
+        
         StringBuilder wordReverse = new StringBuilder(word).reverse();
-
+        
         for (int i = word.length() - 1; i >= 0; i--) {
+
             if (!Character.isLetter(wordReverse.charAt(i))) {
+
                 wordReverse.deleteCharAt(i);
             }
         }
+
         for (int i = 0; i < word.length(); i++) {
+
             if (!Character.isLetter(word.charAt(i))) {
                 wordReverse.insert(i, word.charAt(i));
             }
         }
+        
         return wordReverse;
-    }
+    } 
 }
-
